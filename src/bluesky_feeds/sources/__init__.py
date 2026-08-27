b5bc5b7cc4c16db9bdf2f8d0c4f23ff9c4771f31
FredrikBrink <fredde.brink@outlook.com>
2026-08-27 13:20:00 +0200

    Export DIF source adapters

--- src/bluesky_feeds/sources/__init__.py
+++ src/bluesky_feeds/sources/__init__.py
@@ -1,6 +1,16 @@
 """News source adapters."""
 
 from .base import NewsSource
+from .dif_football import DifFootballSource, parse_dif_football
+from .dif_hockey import DifHockeySource, parse_dif_hockey
 from .rss import RssSource, parse_rss
 
-__all__ = ["NewsSource", "RssSource", "parse_rss"]
+__all__ = [
+    "DifFootballSource",
+    "DifHockeySource",
+    "NewsSource",
+    "RssSource",
+    "parse_dif_football",
+    "parse_dif_hockey",
+    "parse_rss",
+]
